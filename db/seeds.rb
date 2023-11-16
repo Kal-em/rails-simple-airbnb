27ea9b40667c25821028f7be0621ab13f2f6e07d
Robert Chheav <robert.chheav@gmail.com>
2023-11-16 10:03:07 +0100

    index done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Very cozy flat in IJburg',
+  address: 'Nydia Ecurystraat 78 1087VV Amsterdam',
+  description: 'a very new flat in the nice area',
+  price_per_night: 96,
+  number_of_guests: 6
+)
+Flat.create!(
+  name: 'THE penthouse in Centrum Island',
+  address: 'Camille Balystraat 1082XV Amsterdam',
+  description: 'Gigantisc penthouse with swimming pool, sauna and giant kitchen island',
+  price_per_night: 474,
+  number_of_guests: 30
+)
+Flat.create!(
+  name: 'Tiny room in Paris',
+  address: '23 rue de la fourche 75004 Paris',
+  description: 'A miniscule flat, good luck with the mice and the noise hahaha',
+  price_per_night: 35,
+  number_of_guests: 1
+)
